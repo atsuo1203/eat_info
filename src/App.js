f1c0c38bfac1848efec9799611f5ada1bd636784
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-04-28 00:56:00 +0900

    映らない

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import StoreBoard from './StoreBoard';
 
 const data = {
     stores: [
-        { name: "松屋" ,start: "9:00" ,end: "21:00"},
+        { name: "松屋" ,start: "0:00" ,end: "24:00"},
         { name: "武蔵家" ,start: "9:00" ,end: "21:00"}
     ]
 };
@@ -14,12 +14,21 @@ class App extends Component {
   render() {
       const storeBoard = [];
       data.stores.forEach((e) => {
+          const hm = e.end.split(':');
+          const h = parseInt(hm[0]);
+          let end = null;
+          if (h <24){
+              end = moment(e.end, 'HH:mm')
+          } else {
+              end = moment((end-24) + ':' + hm[1], 'HH:mm');
+              end.add('1','d');
+          }
           storeBoard.push(
               <StoreBoard
                   key={e.name}
                   name={e.name}
                   start={moment(e.start, 'HH:mm')}
-                  end={moment(e.end, 'HH:mm')}
+                  end={end}
               />
           )
       });
--- src/StoreBoard.js
+++ src/StoreBoard.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import moment from 'moment'
 
+moment.locale("ja",
+    {
+        relativeTime: {
+            future: 'あと%s',
+            past: '%s前',
+            s: '秒',
+            m: '1分',
+            mm: '%d分',
+            h: '1時間',
+            hh: '%d時間',
+            d: 'un日',
+            dd: '%d日',
+        }
+    });
+
 class StoreBoard extends Component {
+
+    componentDidMount(){
+        const {start,end} = this.props;
+        const isClose = moment.now < start || end < moment.now;
+        this.setState({isClose});
+    }
+
+    renderNextChange() {
+        const {start,end} = this.props;
+        const {isClose} = this.state;
+        if (isClose){
+            return (
+                <p>開店まで {start.fromNow}</p>
+            );
+        }
+            return (
+                <p>閉店まで {end.fromNow}</p>
+            );
+    }
+
     render() {
         const {name,start,end} = this.props;
         const format ='HH時mm分';
-        const isClose = start < moment.now;
+        const isClose = moment.now < start || end < moment.now;
         return (
             <div className="StoreBoard" style={{width:"200px",margin:"10px",background: isClose ? 'red' : '#aaa'}}>
                 <h2>{name}</h2>
-                <p>開店まで {start.fromNow() }</p>
+                {this.renderNextChange()}
                 <p>{start.format(format)} - {end.format(format)}</p>
             </div>
         );
